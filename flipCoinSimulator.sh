852be03becfe7cd03272a5984967f9e482dc2a51
Anshul Jain <anshuljain21120@gmail.com>
2020-07-14 17:29:23 +0530

    add flip_a_coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,14 @@
 #! /bin/bash -x
 echo "Welcome to Flip Coin Simulation!"
+
+function flip_a_coin()
+{
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		echo "Head";
+	else
+		echo "Tail";
+	fi
+}
+
+echo "$( flip_a_coin )";
