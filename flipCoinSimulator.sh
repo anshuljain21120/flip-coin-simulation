fc5189360bb21cc7602fdf0fa6290c5b86012a35
Anshul Jain <anshuljain21120@gmail.com>
2020-07-14 17:49:24 +0530

    flip till either of them win

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -12,11 +12,28 @@ function flip_a_coin()
 }
 function keep_tossing()
 {
-	local limit=${1:-10};
-	for (( counter=1; counter<=limit; counter++ ))
+	local limit=${1:-21};
+	echo "@Initiating flipping sequence";
+	heads_score=1
+	tails_score=1
+	printf "\n";
+	while [ $heads_score -lt $limit -a $tails_score -lt $limit ]
 	do
-		echo "$( flip_a_coin )";
+		if [ $( flip_a_coin ) == "Head" ]; then
+			((heads_score++));
+		else
+			((tails_score++));
+		fi
 	done
+	printf "Heads %5s | Tails %5s\n" $heads_score $tails_score;
+	if [ $heads_score -gt $tails_score ]
+	then
+		echo "Heads won by $((heads_score - tails_score ))";
+	else
+		echo "Tails won by $((tails_score - heads_score ))";
+	fi
 }
 
-echo "$( keep_tossing )";
+max_score=21;
+echo "$( keep_tossing $max_score )";
+
